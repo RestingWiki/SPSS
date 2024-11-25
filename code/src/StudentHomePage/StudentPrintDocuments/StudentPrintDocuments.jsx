73727de4b5d79512911cbcdb4e9c8ebf280548ad
RestingWiki <126898951+RestingWiki@users.noreply.github.com>
2024-11-25 22:31:26 +0700

    feat: Created framework for implementing the Student UI

---
+++ code/src/StudentHomePage/StudentPrintDocuments/StudentPrintDocuments.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Metadata from '../../Metadata/Metada';
+import StudentNavBar from '../StudentNavBar/StudentNavBar';
+
+function StudentPrintDocuments(){
+    return(
+        <>
+            <Metadata/>
+            <StudentNavBar/>
+            <>
+                <h1>TODO</h1>
+            </>   
+        </>
+    )
+}
+
+export default StudentPrintDocuments
--- code/src/main.jsx
+++ code/src/main.jsx
@@ -8,6 +8,17 @@ import SPSOHomePage from './SPSOHomePage/SPSOHoMEPage.jsx'
 import SPSOSystemConfig from './SPSOHomePage/SPSOSystemConfig/SPSOSystemConfig.jsx'
 import SPSOPrinterManagement from './SPSOHomePage/SPSOPrinterManagement/SPSOPrinterManagement.jsx'
 import SPSOLogs from './SPSOHomePage/SPSOLogs/SPSOLogs.jsx'
+
+
+import StudentLoginPage from './StudentLoginPage/StudentLoginPage.jsx'
+import StudentHomePage from './StudentHomePage/StudentHomePage.jsx'
+import StudentUploadDocuments from './StudentHomePage/StudentUploadDocuments/StudentUploadDocuments.jsx'
+import StudentInformation from './StudentHomePage/StudentInformation/StudentInformation.jsx'
+import StudentBuyPaper from './StudentHomePage/StudentBuyPaper/StudentBuyPaper.jsx'
+import StudentPrintDocuments from './StudentHomePage/StudentPrintDocuments/StudentPrintDocuments.jsx'
+import StudentViewPrinter from './StudentHomePage/StudentViewPrinter/StudentViewPrinter.jsx'
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
@@ -44,6 +55,36 @@ const router = createBrowserRouter([
   {
     path: '/SPSOHomePage/SPSOLogs',
     element: <SPSOLogs/>,
+  },
+  // Adding student pages
+  // Explicitly named student paths
+  {
+    path: '/StudentLoginPage',
+    element: <StudentLoginPage/>,
+  },
+  {
+    path: '/StudentHomePage',
+    element: <StudentHomePage />,
+  },
+  {
+    path: '/StudentHomePage/StudentInformation',
+    element: <StudentInformation />,
+  },
+  {
+    path: '/StudentHomePage/StudentUploadDocuments',
+    element: <StudentUploadDocuments />,
+  },
+  {
+    path: '/StudentHomePage/StudentBuyPaper',
+    element: <StudentBuyPaper />,
+  },
+  {
+    path: '/StudentHomePage/StudentPrintDocuments',
+    element: <StudentPrintDocuments />,
+  },
+  {
+    path: '/StudentHomePage/StudentViewPrinter',
+    element: <StudentViewPrinter />,
   },
    
 ]);
